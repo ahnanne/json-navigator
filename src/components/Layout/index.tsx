6704c8640940462846f41144ac6f3ceb2e5c7c93
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-29 23:38:25 +0900

    [U] wasm 사용을 위한 함수 추가
    - 컴파일 에러(expected magic word 00 61 73 6d, found 3c 21 44 4f @+0) 해결 필요

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import initSwc, { transformSync } from '@swc/wasm-web';
+import { SnowCanvas } from "@yein/snowflakes";
 
 import * as Styled from "./style";
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC = ( { children } ) => {
+  const [ snowCanvas, setSnowCanvas ] = useState<null | SnowCanvas>( null );
+
+  // TODO: detect browser
+  useEffect( () => {
+    const instantiateStreaming = async () => {
+      const wasm = await fetch( 'snowflakes_bg.wasm' );
+      const arrayBuffer = await wasm.arrayBuffer();
+      const instantiatedObject = await WebAssembly.instantiate( arrayBuffer );
+      const exportedFunc = instantiatedObject.instance.exports.exported_func as CallableFunction;
+      const test = exportedFunc();
+
+      console.log(test)
+    };
+
+    instantiateStreaming();
+  }, [] );
+
   return <Styled.Section>{children}</Styled.Section>;
 };
 
