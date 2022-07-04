aa60ca79e16f6cb03f9829b57e3476b09f80cc78
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-07-04 23:28:10 +0900

    [U] 롤백

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,27 +1,8 @@
-import React, { useState, useEffect } from "react";
-// import initSwc, { transformSync } from '@swc/wasm-web';
-import { SnowCanvas } from "@yein/snowflakes";
+import React from "react";
 
 import * as Styled from "./style";
 
-const Layout: React.FC = ( { children } ) => {
-  const [ snowCanvas, setSnowCanvas ] = useState<null | SnowCanvas>( null );
-
-  // TODO: detect browser
-  useEffect( () => {
-    const instantiateStreaming = async () => {
-      const wasm = await fetch( 'snowflakes_bg.wasm' );
-      const arrayBuffer = await wasm.arrayBuffer();
-      const instantiatedObject = await WebAssembly.instantiate( arrayBuffer );
-      const exportedFunc = instantiatedObject.instance.exports.exported_func as CallableFunction;
-      const test = exportedFunc();
-
-      console.log(test)
-    };
-
-    instantiateStreaming();
-  }, [] );
-
+const Layout: React.FC = ({ children }) => {
   return <Styled.Section>{children}</Styled.Section>;
 };
 
