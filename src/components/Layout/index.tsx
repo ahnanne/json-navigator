8de5fd7403f1cbe9fa25c720b22a6889cc05794b
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-07-06 23:01:49 +0900

    [A] wasm load function
    TODO:
    - magic header issue
    - MIME type issue

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import * as Styled from "./style";
 
 const Layout: React.FC = ({ children }) => {
+  const [wasm, setWasm] = useState<null | any>(null); // TODO:
+
+  useEffect(() => {
+    (async () => {
+      const snowflakes = await import("./snowflakes");
+      console.log(snowflakes);
+      setWasm(snowflakes);
+    })();
+  }, []);
+
+  console.log(wasm);
+
   return <Styled.Section>{children}</Styled.Section>;
 };
 
